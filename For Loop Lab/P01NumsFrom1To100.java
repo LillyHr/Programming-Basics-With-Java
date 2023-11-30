02333f6d124606101fb67e34dc6bf407d9e0dd4d
Lili Hristova <122538791+LillyHr@users.noreply.github.com>
2023-11-30 23:50:00 +0200

    Create P01NumsFrom1To100.java

---
+++ For Loop Lab/P01NumsFrom1To100.java
@@ -0,0 +1,13 @@
+package ForLoopJava;
+
+import java.util.Scanner;
+
+public class P01NumsFrom1To100 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 1; i<= 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
