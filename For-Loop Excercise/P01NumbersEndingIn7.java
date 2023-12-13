b1022500325a30f6d48b43e5c548802bcb8b15b3
Lili Hristova <122538791+LillyHr@users.noreply.github.com>
2023-12-13 23:17:22 +0200

    Create P01NumbersEndingIn7.java

---
+++ For-Loop Excercise/P01NumbersEndingIn7.java
@@ -0,0 +1,13 @@
+package ForLoopJavaExercise;
+
+import java.util.Scanner;
+
+public class P01NumbersEndingIn7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        for (int i = 7; i <= 1000; i = i + 10) {
+            System.out.println(i);
+        }
+    }
+}
