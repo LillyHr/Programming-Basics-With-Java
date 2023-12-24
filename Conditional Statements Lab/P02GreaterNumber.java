4057bf387d61dde0ad895c4f7b45705a60359b4c
Lili Hristova <122538791+LillyHr@users.noreply.github.com>
2023-12-25 00:26:49 +0200

    Create P02GreaterNumber.java

---
+++ Conditional Statements Lab/P02GreaterNumber.java
@@ -0,0 +1,17 @@
+package ConditionalStatementsLab;
+
+import java.util.Scanner;
+
+public class P02GreaterNumber {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num1 = Integer.parseInt(scanner.nextLine());
+        int num2 = Integer.parseInt(scanner.nextLine());
+
+        if (num1 >= num2) {
+            System.out.println(num1);
+        } else if (num1 <= num2) {
+            System.out.println(num2);
+        }
+    }
+}
